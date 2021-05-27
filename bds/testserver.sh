9fe2243d1251cd7ce37e8c41c935d9ae4dbd91a0
KAIYOHUGO <pink+KAIYOHUGO@users.noreply.github.com>
2021-05-27 20:43:43 +0800

    🔥add server installer

--- bds/testserver.sh
+++ bds/testserver.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+wget $(wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36" https://minecraft.net/en-us/download/server/bedrock/ -O - 2>/dev/null | grep -o 'https://minecraft.azureedge.net/bin-linux/[^"]*') -O update.zip
+unzip update.zip ./bds/
 echo server start
-read a
-echo server stop = $a
+LD_LIBRARY_PATH=. ./bedrock_server
